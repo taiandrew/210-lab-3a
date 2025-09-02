2fd78613297da98f40a220b528d7daee71ffb741
taiandrew <31713490+taiandrew@users.noreply.github.com>
2025-09-02 13:26:10 -0700

    Preamble

--- tai_lab3a.cpp
+++ tai_lab3a.cpp
@@ -1,3 +1,28 @@
 // COMSC 210 | Andrew Tai | Sep 2 2025
 // IDE: Visual Studio Code
 
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// Structure for Restaurant
+struct Restaurant
+{
+    string name;
+    string city;
+    int seating;
+    string cuisine;
+    float rating;
+};
+
+// Function prototypes
+Restaurant getRestaurantData();
+void displayRestaurant(const Restaurant &r);
+
+int main() 
+{
+
+    return 0;
+}
+
